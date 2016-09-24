542cd4df066ebd544aa931db9aaddb0d31fb454b
louhichio <louhichio@hotmail.com>
2016-09-25 01:40:39 +0200

    Always Jump + DoubleJump

--- Assets/FloppyJump/2_Scripts/Floppy.cs
+++ Assets/FloppyJump/2_Scripts/Floppy.cs
@@ -1,36 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 [RequireComponent (typeof (Rigidbody2D))]
 public class Floppy : MonoBehaviour 
 {
+	#region Properties
 	public float jumpForce = 100;
 	public float falldownSpeed = 1.5f;
 	public float swingSpeed = 1.5f;
-	public float gravity = 1.0f;
 
-	public bool isGrounded = false;
+	private bool isGrounded = false;
 	public bool InfiniteJump = false;
+	public bool doubleJump = false;
+	private bool djDone = false;
 
 	private Rigidbody2D rb;
-	private MeshRenderer renderer;
-
-	public Vector3 moveDirection = Vector3.zero;
+	private MeshRenderer rend;
+	private Collider2D groundedObj;
 
 	int layerMask = 1 << 8;
 
-	private List<Collider2D> colliders;
+	private Vector3 origin;
+	private Vector3 size;
 
+
+	#endregion
+
+	#region Unity
 	void OnDrawGizmosSelected() 
 	{
-		renderer = GetComponent<MeshRenderer> ();
+		rend = GetComponent<MeshRenderer> ();
 
-		Vector3 origin = transform.position;
-		origin.y -= renderer.bounds.size.y / 2;
-
-		Vector3 size = renderer.bounds.size;
-		size.x -= 0.1f;
+		origin = transform.position;
+		origin.y -= rend.bounds.size.y / 2;
 
 		Gizmos.color = new Color(1, 0, 0, 0.5F);
 
@@ -39,37 +43,65 @@ public class Floppy : MonoBehaviour
 
 	void Start () 
 	{
-		renderer = GetComponent<MeshRenderer> ();
 		rb = GetComponent<Rigidbody2D> ();
-		colliders = new List<Collider2D> ();
+		rend = GetComponent<MeshRenderer> ();
+
+		size = rend.bounds.size;
+		size.x -= 0.1f;
+		size.y = 0.50f;
 	}
 
 	void FixedUpdate () 
 	{    
-		CheckIsGrounded ();
-
 		if (Input.GetKeyDown ("up")) 
 		{
-			if(InfiniteJump)
+			if (InfiniteJump || isGrounded)
 				Jump ();
-			else if (isGrounded)
+			else if (doubleJump && !djDone) 
+			{
+				djDone = true;
 				Jump ();
+			}
 		}
 
 		if (Input.GetKeyDown ("down") && !isGrounded) 
-		{
-			FallDown ();			
-		}
+			Fall ();			
 
 		Swing ();
 	}
 
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if(!isGrounded)
+			CheckIsGrounded (coll.collider);
+	}
+
+	void OnCollisionStay2D(Collision2D coll) 
+	{
+		if(!isGrounded)
+			CheckIsGrounded (coll.collider);
+	}
+
+	void OnCollisionExit2D(Collision2D coll) 
+	{
+		if (coll.collider == groundedObj) 
+		{
+			isGrounded = false;
+			djDone = false;
+		}
+	}
+	#endregion
+
+
 	private void Jump()
 	{
+		Vector3 vel = rb.velocity;
+		vel.y = 0;
+		rb.velocity = vel;
 		rb.AddForce (Vector2.up * jumpForce);
 	}
 
-	private void FallDown()
+	private void Fall()
 	{
 		Vector3 vel = rb.velocity;
 		vel.y = 0;
@@ -82,28 +114,18 @@ public class Floppy : MonoBehaviour
 		rb.AddForce(Vector3.right * Mathf.Cos (Time.time) * swingSpeed);
 	}
 
-	void OnCollisionEnter2D(Collision2D coll) 
+	private void CheckIsGrounded(Collider2D coll)
 	{
-		if (!colliders.Contains (coll.collider))
-			colliders.Add (coll.collider);
-	}
-	void OnCollisionExit2D(Collision2D coll) 
-	{
-		colliders.Remove (coll.collider);
-	}
-
-	private void CheckIsGrounded()
-	{
-		Vector3 origin = transform.position;
-		origin.y -= renderer.bounds.size.y / 2;
-
-		Vector3 size = renderer.bounds.size;
-		size.x -= 0.1f;
+		origin = transform.position;
+		origin.y -= rend.bounds.size.y / 4;
 
 		Collider2D detectedGroundObj = Physics2D.OverlapBox (origin, size, 0, ~layerMask);
 
-		isGrounded = colliders.Contains(detectedGroundObj);
-
-//		print (isGrounded + "  " + Physics2D.OverlapBox (origin, size, 0, ~layerMask));
+		if (detectedGroundObj && coll == detectedGroundObj) 
+		{
+			groundedObj = coll;
+			isGrounded = true;
+//			print (rend.bounds.Intersects (detectedGroundObj.bounds) + "  " + detectedGroundObj);
+		}
 	}
 }
