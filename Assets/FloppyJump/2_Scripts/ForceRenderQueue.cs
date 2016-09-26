63621f28c5766169083b9ac219247348284d7ee6
cosmografik <bretzel75@gmail.com>
2016-09-26 11:31:45 +0200

    Polish

---
+++ Assets/FloppyJump/2_Scripts/ForceRenderQueue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Renderer))]
+public class ForceRenderQueue : MonoBehaviour
+{
+    public enum UsedQueue { Background = 1000, Geometry = 2000, AlphaTests = 2450, Transparent = 3000, Overlay = 4000 };
+    public UsedQueue baseQueue = UsedQueue.Background;
+    public int renderQueueIndex = 0;
+
+    
+}
