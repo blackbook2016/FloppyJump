c0920a9760b97247673b22dcaf8a584d1db1d61a
louhichio <louhichio@hotmail.com>
2016-09-26 10:45:18 +0200

    Menu Fade

--- Assets/FloppyJump/2_Scripts/Menu.cs
+++ Assets/FloppyJump/2_Scripts/Menu.cs
@@ -1,12 +1,39 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Menu : MonoBehaviour 
 {
-	void Update () 
+	public Image fade;
+	public float fadeTimer = 5;
+
+	private bool changeSceneStarted = false;
+
+	private Color c;
+	private float timerStart;
+
+	void Start()
 	{
-		if (Input.GetKeyDown ("up")) 
-			SceneManager.LoadScene ("Scene_1");
+		c = fade.color;
+		c.a = 0;
+		fade.color = c;
+	}
+
+	void Update ()
+	{
+		if (!changeSceneStarted && Input.GetKeyDown ("up")) 
+		{
+			timerStart = Time.time;
+			changeSceneStarted = true;
+		}
+		else if(changeSceneStarted)
+		{
+			c.a =  ((Time.time - timerStart) / fadeTimer);
+			fade.color = c;
+
+			if (c.a >= 1)
+				SceneManager.LoadScene ("Scene_1");				
+		}
 	}
 }
